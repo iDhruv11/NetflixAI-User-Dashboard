aa3e3969cc3fc83ff4fd5ad9f0fd250c88b2629f
dhruv <drvsrm11@gmail.com>
2024-12-14 15:57:18 +0530

    env file configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,23 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import globals, { browser } from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+
 
 export default [
   { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
+    env: {
+      browser: true,
+      node: true,
+    },
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import dotenv from 'dotenv'
 
-dotenv.config()
+// dotenv.config()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
